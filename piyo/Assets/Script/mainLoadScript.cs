f361b02fc7fb8a485cd2cdb0938dcdac3ba5dfcc
yuuta <poke.d.p.0931-y.s@docomo.ne.jp>
2016-02-05 19:06:28 +0900

    0205

---
+++ piyo/Assets/Script/mainLoadScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine; 
+using System.Collections; 
+ 
+
+ public class mainLoadScript : MonoBehaviour { 
+	 
+
+	 	public void LoadMain() { 
+		 		Application.LoadLevel ("piyo"); 
+		 	} 
+	 } 
